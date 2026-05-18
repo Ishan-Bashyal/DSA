0c4d48910c42eba06808aa8d6252ffff53fb7ad5
ishan <ishanbashyal11@gmail.com>
2026-05-18 23:12:15 +0545

    Simple Stack Code With Different Functions

--- stack.py
+++ stack.py
@@ -30,3 +30,37 @@ class Stack:
             return None
         print("Top element:", self.stack[-1])
         return self.stack[-1]
+
+    def display(self):
+        if self.is_empty():
+            print("Stack is empty")
+        else:
+            print("Stack (top to bottom):", self.stack[::-1])
+
+
+s = Stack()
+
+while True:
+    print("\n--- Stack Menu ---")
+    print("1. Push")
+    print("2. Pop")
+    print("3. Peek")
+    print("4. Display")
+    print("5. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "1":
+        item = input("Enter value to push: ")
+        s.push(item)
+    elif choice == "2":
+        s.pop()
+    elif choice == "3":
+        s.peek()
+    elif choice == "4":
+        s.display()
+    elif choice == "5":
+        print("Bye!")
+        break
+    else:
+        print("Invalid choice, try again")
